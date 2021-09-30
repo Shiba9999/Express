4f2bd909273b2047089c417e0cfddea740ccb506
shiba <rp.shiba2@gmail.com>
2021-10-01 01:14:31 +0530

    error

--- server.js
+++ server.js
@@ -9,13 +9,51 @@ app.get("/",(req,res)=>{
     res.send("hello from backend");
 })
  app.use(express.json())//just add this line before get or post req IMP
+ let user=[]
+ app.use(express.static("public"))
+const userRouter=express.Router();
+const authRouter=express.Router();
+app.use("/user",userRouter)
+app.use("/auth",authRouter)
+//mounting in express
+userRouter
+.route("/")
+.get(getUser)
+.post()
+.patch()
+.delete()
 
-let user={
-   
+authRouter
+.route("/signup")
+.post(signupUser)
+
+
+function signupUser (req,res){
+    let userDetails=req.body
+    // let {email,name,password}=req.body
+    user.push({email,name,password});
+    let name=userDetails.name;
+    let email=userDetails.email
+    let password=userDetails.password
+    console.log(req.body)
+    res.json({
+        message:"user signed up",
+        user:req.body
+    })
 }
-app.get("/user",(req,res)=>{
-res.send(user);
-})
+// for param route
+
+// userRouter
+// .route("/:id")
+// .get
+
+// app.get("/user",getUser)
+
+function getUser(req,res){
+    res.send(user);
+    }
+//simillarly we can convert post patch and delete like get req 
+
 app.post("/user",(req,res)=>{
     user=req.body;
     console.log(req.body)
@@ -34,4 +72,10 @@ app.patch("/user",(req,res)=>{
 app.delete("/user",(req,res)=>{
     user={}
     res.json(user);
+})
+
+//parameter route
+app.get("/user/:id",(req,res)=>{
+    console.log(req.params);
+    res.send(req.params);
 })
