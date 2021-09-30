795076479a5c20799701a9af3a5ebb9d6e7345a1
shiba <rp.shiba2@gmail.com>
2021-10-01 00:10:00 +0530

    all req

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 let express=require("express");
 let app=express();
-let PORT="8080"
+let PORT="5000"
 app.listen(PORT,function(){
     console.log(`app is running at port ${PORT}`);
 })
@@ -8,15 +8,30 @@ app.get("/",(req,res)=>{
     console.log(req.path);
     res.send("hello from backend");
 })
+ app.use(express.json())//just add this line before get or post req IMP
 
-let obj={
-    name:"shiba"
+let user={
+   
 }
 app.get("/user",(req,res)=>{
-res.send(obj);
-
+res.send(user);
 })
-app.get("/file",(req,res)=>{
-    console.log(__dirname);
-    res.sendFile( __dirname + "/public/" + "index.html" );
+app.post("/user",(req,res)=>{
+    user=req.body;
+    console.log(req.body)
+    res.send("data added succesfully")
+})
+app.patch("/user",(req,res)=>{
+    let obj=req.body
+    for(let key in obj){
+        user[key]=obj[key]
+
+    }
+    res.json(user);
+  //for every time server start start using all the req once again
+    //for patch first post then check get then patch to add 
+})
+app.delete("/user",(req,res)=>{
+    user={}
+    res.json(user);
 })
